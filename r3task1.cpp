5ed0145f5ccb54ef9359176dbef4f72322e69036
Awedtan <31903483+Awedtan@users.noreply.github.com>
2021-09-29 13:57:25 -0400

    Update r3task1.cpp

--- r3task1.cpp
+++ r3task1.cpp
@@ -1,17 +1,18 @@
-// Daniel Su, Sept 28, R3 software task
+// Daniel Su, Sept 28, R3 software task 1
 
-const int TENS_FIRST_OUTPUT = 5; // BCD output pins
-const int TENS_SECOND_OUTPUT = 4;
-const int TENS_THIRD_OUTPUT = 3;
-const int TENS_FOURTH_OUTPUT = 2;
-const int ONES_FIRST_OUTPUT = 9;
-const int ONES_SECOND_OUTPUT = 8;
-const int ONES_THIRD_OUTPUT = 7;
-const int ONES_FOURTH_OUTPUT = 6;
+// Output pin numbers
+const int TENS_FIRST_OUTPUT = 9;
+const int TENS_SECOND_OUTPUT = 8;
+const int TENS_THIRD_OUTPUT = 7;
+const int TENS_FOURTH_OUTPUT = 6;
+const int ONES_FIRST_OUTPUT = 5;
+const int ONES_SECOND_OUTPUT = 4;
+const int ONES_THIRD_OUTPUT = 3;
+const int ONES_FOURTH_OUTPUT = 2;
 
 void setup()
 {
-	Serial.begin(9600);
+  	// Set all the output pins
 	pinMode(TENS_FOURTH_OUTPUT, OUTPUT);
 	pinMode(TENS_THIRD_OUTPUT, OUTPUT);
 	pinMode(TENS_SECOND_OUTPUT, OUTPUT);
@@ -24,45 +25,51 @@ void setup()
 
 void loop()
 {
+	// Get the input from the potentiometer and map it to a range of values from 0-99
 	int adc = analogRead(5);
 	adc = map(adc, 0, 1023, 0, 99);
 
-	int first = adc / 10;
-	int second = adc % 10;
-
-	bool firstDigits[] = {first / 8 == 1, first / 4 % 2 == 1, first / 2 % 2 == 1, first % 2 == 1};
-	bool secondDigits[] = {second / 8 == 1, second / 4 % 2 == 1, second / 2 % 2 == 1, second % 2 == 1};
-
-	if (firstDigits[0])
+	// Get the individual tens and ones digits
+	int tens = adc / 10;
+	int ones = adc % 10;
+	
+	// Turn the tens and ones digits into binary
+	// 7 -> 0111 -> [false, true, true, true]
+	bool tensDigits[] = {tens / 8 == 1, tens / 4 % 2 == 1, tens / 2 % 2 == 1, tens % 2 == 1};
+	bool onesDigits[] = {ones / 8 == 1, ones / 4 % 2 == 1, ones / 2 % 2 == 1, ones % 2 == 1};
+	
+	// Sets the output pins to the tens digit decoder to be high or low
+	if (tensDigits[0])
 		digitalWrite(TENS_FIRST_OUTPUT, HIGH);
 	else
 		digitalWrite(TENS_FIRST_OUTPUT, LOW);
-	if (firstDigits[1])
+	if (tensDigits[1])
 		digitalWrite(TENS_SECOND_OUTPUT, HIGH);
 	else
 		digitalWrite(TENS_SECOND_OUTPUT, LOW);
-	if (firstDigits[2])
+	if (tensDigits[2])
 		digitalWrite(TENS_THIRD_OUTPUT, HIGH);
 	else
 		digitalWrite(TENS_THIRD_OUTPUT, LOW);
-	if (firstDigits[3])
+	if (tensDigits[3])
 		digitalWrite(TENS_FOURTH_OUTPUT, HIGH);
 	else
 		digitalWrite(TENS_FOURTH_OUTPUT, LOW);
-		
-	if (secondDigits[0])
+	
+	// Sets the output pins to the ones digit decoder to be high or low
+	if (onesDigits[0])
 		digitalWrite(ONES_FIRST_OUTPUT, HIGH);
 	else
 		digitalWrite(ONES_FIRST_OUTPUT, LOW);
-	if (secondDigits[1])
+	if (onesDigits[1])
 		digitalWrite(ONES_SECOND_OUTPUT, HIGH);
 	else
 		digitalWrite(ONES_SECOND_OUTPUT, LOW);
-	if (secondDigits[2])
+	if (onesDigits[2])
 		digitalWrite(ONES_THIRD_OUTPUT, HIGH);
 	else
 		digitalWrite(ONES_THIRD_OUTPUT, LOW);
-	if (secondDigits[3])
+	if (onesDigits[3])
 		digitalWrite(ONES_FOURTH_OUTPUT, HIGH);
 	else
 		digitalWrite(ONES_FOURTH_OUTPUT, LOW);
